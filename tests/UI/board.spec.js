13f5cbb3c01326acfad69899327ada4d453f9fc0
smarquezdiaz <smarquez@servicios.mc4.com.bo>
2025-10-06 03:19:11 -0400

    fix allure tag

--- tests/UI/board.spec.js
+++ tests/UI/board.spec.js
@@ -6,17 +6,23 @@ var randomstring = require("randomstring");
 test.describe('Suite de pruebas para crear tablero', () => {
     let boardTitle = randomstring.generate(5);
     test.use(boardTitle);
-    test('Verificar creacion exitosa de tablero', async ({boardPage}) => {
+    test('Verificar creacion exitosa de tablero', async ({ boardPage }) => {
         allure.tag('UI');
         allure.owner('Sol Abril Marquez Diaz');
-        allure.severity('smoke');
+        allure.severity('smoke, regression, positive');
         await boardPage.createBoard(boardTitle);
     })
-    test('Verificar que no permita crear un tablero con titulo vacio', async ({boardPage}) => {
+    test('Verificar que no permita crear un tablero con titulo vacio', async ({ boardPage }) => {
+        allure.tag('UI');
+        allure.owner('Sol Abril Marquez Diaz');
+        allure.severity('smoke, regression, negative');
         let boardTitle = "";
         await boardPage.createBoard(boardTitle);
     })
-    test('Verificar que no permita crear un tablero con titulo largo', async ({boardPage}) => {
+    test('Verificar que no permita crear un tablero con titulo largo', async ({ boardPage }) => {
+        allure.tag('UI');
+        allure.owner('Sol Abril Marquez Diaz');
+        allure.severity('smoke, regression, negative');
         let boardTitle = randomstring.generate(16385);
         await boardPage.createBoard(boardTitle);
     })
@@ -25,18 +31,18 @@ test.describe('Suite de pruebas para crear tablero', () => {
 test.describe('Suite de pruebas para editar tablero', () => {
     let updateTitle = randomstring.generate(5);
     test.use(updateTitle);
-    test('Verificar edicion exitosa de tablero', async ({updatePage}) => {
+    test('Verificar edicion exitosa de tablero', async ({ updatePage }) => {
         allure.tag('UI');
         allure.owner('Sol Abril Marquez Diaz');
-        allure.severity('smoke');
+        allure.severity('smoke, regression, positive');
         const boardPage = updatePage.pageObject;
         const initialTitle = updatePage.initialTitle;
         await boardPage.updateBoard(initialTitle, updateTitle);
     })
-    test('Verificar que no permita editar un tablero con titulo vacio', async ({updatePage}) => {
+    test('Verificar que no permita editar un tablero con titulo vacio', async ({ updatePage }) => {
         allure.tag('UI');
         allure.owner('Sol Abril Marquez Diaz');
-        allure.severity('smoke');
+        allure.severity('smoke, regression, negative');
         let updateTitle = "";
         const boardPage = updatePage.pageObject;
         const initialTitle = updatePage.initialTitle;
@@ -45,10 +51,10 @@ test.describe('Suite de pruebas para editar tablero', () => {
 })
 
 test.describe('Suite de pruebas para poner en favoritos un tablero', () => {
-    test('Verificar que permita poner un tablero en favoritos', async ({favoritePage}) => {
+    test('Verificar que permita poner un tablero en favoritos', async ({ favoritePage }) => {
         allure.tag('UI');
         allure.owner('Sol Abril Marquez Diaz');
-        allure.severity('smoke');
+        allure.severity('smoke, regression, positive');
         const boardPage = favoritePage.pageObject;
         const initialTitle = favoritePage.initialTitle;
         await boardPage.addFavorite(initialTitle);
@@ -56,10 +62,10 @@ test.describe('Suite de pruebas para poner en favoritos un tablero', () => {
 })
 
 test.describe('Test para flujo de de crear, editar y eliminar un tablero', () => {
-    test('Verificar flujo de crear, editar y eliminar un tablero', async ({initPage}) => {
+    test('Verificar flujo de crear, editar y eliminar un tablero', async ({ initPage }) => {
         allure.tag('UI');
         allure.owner('Sol Abril Marquez Diaz');
-        allure.severity('smoke');
+        allure.severity('e2e');
         let boardTitle = randomstring.generate(5);
         let updateTitle = randomstring.generate(5);
         await initPage.createBoard(boardTitle);
